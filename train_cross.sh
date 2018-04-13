68d98a78f8e7e6a56c787fda56ca6d966bf6aac8
Hoài Văn <phvan2312@gmail.com>
2018-04-13 11:15:39 +0700

    Update train_cross.sh

--- train_cross.sh
+++ train_cross.sh
@@ -1,11 +1,12 @@
 saved_result_dir='./Results/13042018'
 
-for (( i=0; i <= 4; ++i ))
+max=4
+for (( i=2; i <= $max; ++i ))
 do
-	fold_i="fold_$i"
-	echo "========================================================="
-	echo "==================== running $fold_i ====================="
-	echo "========================================================="
-
-	python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
+    fold_i="fold_$i"
+    echo "========================================================="
+    echo "==================== running $fold_i ====================="
+    echo "========================================================="
+    
+    python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
 done
