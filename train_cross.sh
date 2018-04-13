280c87985ff47e7aa60f5249d251b213cb38a1da
Hoài Văn <phvan2312@gmail.com>
2018-04-13 11:20:19 +0700

    Update train_cross.sh

--- train_cross.sh
+++ train_cross.sh
@@ -1,12 +1,41 @@
 saved_result_dir='./Results/13042018'
 
-max=4
-for (( i=0; i <= $max; ++i ))
-do
-    fold_i="fold_$i"
-    echo "========================================================="
-    echo "==================== running $fold_i ====================="
-    echo "========================================================="
+i=0
+fold_i="fold_$i"
+echo "========================================================="
+echo "==================== running $fold_i ====================="
+echo "========================================================="
     
-    python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
-done
+python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
+
+i=1
+fold_i="fold_$i"
+echo "========================================================="
+echo "==================== running $fold_i ====================="
+echo "========================================================="
+    
+python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
+
+i=2
+fold_i="fold_$i"
+echo "========================================================="
+echo "==================== running $fold_i ====================="
+echo "========================================================="
+    
+python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
+
+i=3
+fold_i="fold_$i"
+echo "========================================================="
+echo "==================== running $fold_i ====================="
+echo "========================================================="
+    
+python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
+
+i=4
+fold_i="fold_$i"
+echo "========================================================="
+echo "==================== running $fold_i ====================="
+echo "========================================================="
+    
+python train.py --word2vec_path="./Data/Word2vec/43k_word2vec.bin" --train_data_path="./Data/Train/$fold_i/train.csv" --train_data_path="./Data/Train/$fold_i/test.csv" --epochs=30 --freq_eval=20 --model_params_path="./model_params.json" --batch_size=25 --saved_result_path="$saved_result_dir/$fold_i"
